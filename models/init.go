3351b20f4f1412784378079cba3855fa272386e6
Venator <15882190474@163.com>
2020-03-26 21:08:44 +0800

    <fix>Card.go/ModifiCardInfo() &  UseScore()

--- models/init.go
+++ models/init.go
@@ -37,7 +37,6 @@ func init() {
 	//下面要修改，根据数据库设计
 	//zjn
 	orm.RegisterModel(new(Card)) //登记orm
-	orm.RegisterModel(new(Coupons))
 	orm.RegisterModel(new(Enterprise))
 	orm.Debug = true
 	_ = orm.RunSyncdb("default", false, true)
